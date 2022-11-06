6e4bf415fe3eb84f48b0b00bdf409a7a0116e441
Yash Nishikant <yash0312@hotmail.com>
2022-11-05 20:53:06 -0400

    update opencv

--- OpenCV/camdetection.py
+++ OpenCV/camdetection.py
@@ -4,6 +4,10 @@ import numpy as np
 time_2 = 0
 video = cv2.VideoCapture(0)
 ret, prevFrame = video.read()
+overlap_image = np.zeros((720, 1280), dtype=np.uint8)
+movement = np.zeros((720,1280), dtype=np.uint8)
+brightness = np.zeros((720,1280), dtype=np.uint8)
+
 
 while True:
     
@@ -12,15 +16,20 @@ while True:
     if cv2.waitKey(1)  & 0xFF == ord('q'):
         break
 
-
     movement = cv2.absdiff(currFrame, prevFrame)
         
     prevFrame = currFrame
     gray = cv2.cvtColor(currFrame, cv2.COLOR_BGR2GRAY)
+    test = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
 
-    brightness = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    _, movementthresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    _, brightness = cv2.threshold(test, 127, 255, cv2.THRESH_BINARY)
 
-    cv2.imshow('frame', movement)
+    print("movement " + str(type(movementthresh)))
+    print("brightness" + str(type(brightness)))
+
+    cv2.bitwise_and(movementthresh, brightness, overlap_image)
+    cv2.imshow('frame', overlap_image)
 
 
 
@@ -28,3 +37,4 @@ while True:
 #convert images to binary
 #bitwise and
 #find overlap
+#np.unoit8
