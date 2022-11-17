de04aead19ac5aaf5f89a9766af13ca0c4dde574
Yash Nishikant <yash0312@hotmail.com>
2022-11-17 00:04:55 -0500

    gesture bugfix

--- OpenCV/camdetectionV3.py
+++ OpenCV/camdetectionV3.py
@@ -17,6 +17,8 @@ run = False
 x = 0
 deltaX = 0
 
+prevContours = []
+currContours = []
 
 def distance(x,y,x1,y1):
     return math.sqrt(pow((x1-x),2) + pow((y1-y),2))
@@ -36,19 +38,24 @@ while video.isOpened():
     if(run):
         diff = cv2.absdiff(thresh, prevFrame)
         contours = cv2.findContours(diff, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
-        
         contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        currContours = contours
 
         if len(contours) >= 1:
             x,y,w,h = cv2.boundingRect(contours[0])
             x = x 
-            deltaX = abs(boxx - x)
+
+            if len(prevContours) > 0: 
+                deltaX = abs(boxx - x)
+            else:
+                deltaX = 0
+            
             if len(contours) >= 2:
                 x2,y2,w2,h2 = cv2.boundingRect(contours[1])
 
             if(cv2.contourArea(contours[0]) > 100):
                 cv2.rectangle(currFrame, (x,y), (x+w, y+h), (0,255,0), 3)   
-            
+
         if len(contours) >= 2 and distance(x,y,x2,y2) > 90:
             if(cv2.contourArea(contours[1]) > 100):
                 cv2.rectangle(currFrame, (x2,y2), (x2+w2, y2+h2), (0,255,0), 3)   
@@ -61,13 +68,18 @@ while video.isOpened():
             time.sleep(1)
             pg.moveTo(1012, 342)
             time.sleep(1)
-            pg.typewrite("https://www.youtube.com/watch?v=0NJf-RRuS2Y")
+            pg.typewrite("https://www.youtube.com/watch?v=HjPgdhRsGIQ")
             pg.press("enter")
 
     cv2.imshow('output', currFrame)
 
-    prevFrame = cv2.threshold(blur, 253, 255, cv2.THRESH_BINARY)[1]
+    prevFrame = cv2.threshold(blur, 253, 255, cv2.THRESH_BINARY)[1]    
+    
+    if(run):
+        prevContours = contours
+
     run = True
+    
     if cv2.waitKey(1)  & 0xFF == ord('q'):
         break
 
