d7d1079cab6113f9109f0e7f04f11328918e1d94
Yash Nishikant <yash0312@hotmail.com>
2022-11-17 16:01:34 -0500

    final changes
    -cooldown timer
    -fixed initial contour detection

--- OpenCV/camdetectionV3.py
+++ OpenCV/camdetectionV3.py
@@ -4,27 +4,32 @@ import math
 import pyautogui as pg
 import time
 
-
-
 video = cv2.VideoCapture(0)
 prevFrame = video.read()[1]
 currFrame = video.read()[1]
 
-tick = 0
-
-run = False
-
-x = 0
-deltaX = 0
-
 prevContours = []
 currContours = []
+
+tick = 0
+x = 0
+deltaX = 0
+t1 = 0
+t2 = 0
+timepassed = 0
+futuretime = 0
+
+run = False
+cooldown = False
 
 def distance(x,y,x1,y1):
     return math.sqrt(pow((x1-x),2) + pow((y1-y),2))
 
 while video.isOpened():
     
+    t1 = time.time()
+    timepassed = timepassed + (t1-t2)
+
     boxx = x
     
     tick = tick+1
@@ -39,6 +44,13 @@ while video.isOpened():
         diff = cv2.absdiff(thresh, prevFrame)
         contours = cv2.findContours(diff, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
         contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        
+        i=len(contours)-1
+        for c in contours:
+            if(cv2.contourArea(contours[i]) < 100):
+                contours = np.delete(contours, i, axis=0)
+            i=i-1
+
         currContours = contours
 
         if len(contours) >= 1:
@@ -60,16 +72,20 @@ while video.isOpened():
             if(cv2.contourArea(contours[1]) > 100):
                 cv2.rectangle(currFrame, (x2,y2), (x2+w2, y2+h2), (0,255,0), 3)   
                 cv2.line(currFrame, (int(x+w/2),int(y+h/2)), (int(x2+w2/2),int(y2+h2/2)), (255,0,0), 3)
-
-        if(deltaX > 200):
+        if(deltaX > 200 and not cooldown):
             pg.moveTo(163,1066)
             pg.leftClick()
-
             time.sleep(1)
             pg.moveTo(1012, 342)
-            time.sleep(1)
-            pg.typewrite("https://www.youtube.com/watch?v=HjPgdhRsGIQ")
+            pg.typewrite("https://github.com/YashNishikant")
             pg.press("enter")
+            cooldown = True
+            futuretime = timepassed + 10
+
+        if timepassed > futuretime:
+            cooldown = False
+        else:
+            cv2.putText(currFrame, str(int(futuretime-timepassed)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,255), 2, cv2.LINE_AA)
 
     cv2.imshow('output', currFrame)
 
@@ -79,6 +95,8 @@ while video.isOpened():
         prevContours = contours
 
     run = True
+    
+    t2 = t1
     
     if cv2.waitKey(1)  & 0xFF == ord('q'):
         break
